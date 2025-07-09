6134cb66b098fa937a2e65e5f2c150651510b745
QanticaTest <test@qantica.com>
2025-07-09 19:45:47 +0200

    Update HeroSection.tsx update logo loading

--- src/components/dashboard/HeroSection.tsx
+++ src/components/dashboard/HeroSection.tsx
@@ -22,6 +22,8 @@ const HeroSection: React.FC<HeroSectionProps> = ({ titleOpacity, titleTransform 
             src="/lovable-uploads/289024cd-7882-4667-84d5-d16efa85e32c.webp" 
             alt="Q Logo" 
             className="mx-auto w-72 h-72 sm:w-96 sm:h-96 md:w-[28rem] md:h-[28rem] lg:w-[32rem] lg:h-[32rem] xl:w-[36rem] xl:h-[36rem] object-contain"
+            loading="eager"   
+            fetchPriority="high"
           />
         </div>
       </div>
