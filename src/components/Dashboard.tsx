171d10b3db66c02826fecb61650525c49e79c052
QanticaTest <test@qantica.com>
2025-07-22 23:13:52 +0200

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,7 +5,7 @@ import { useFileDownload } from '../hooks/useFileDownload';
 import ProgressIndicator from './dashboard/ProgressIndicator';
 import DashboardHeader from './dashboard/DashboardHeader';
 import HeroSection from './dashboard/HeroSection';
-//import TitleSection from './dashboard/TitleSection';
+import TitleSection from './dashboard/TitleSection';
 import DescriptionSection from './dashboard/DescriptionSection';
 import MarketCompetition from './dashboard/MarketCompetition';
 import FilesSection from './dashboard/FilesSection';
@@ -25,7 +25,7 @@ const Dashboard = () => {
       <main className="container mx-auto px-4">
         <HeroSection />
 
-       // <TitleSection />
+       <TitleSection />
         
         <DescriptionSection />
 
