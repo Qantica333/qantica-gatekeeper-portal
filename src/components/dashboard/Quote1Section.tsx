bf1620471367b213d4099575fcceee8fefa425e4
QanticaTest <test@qantica.com>
2025-07-22 23:49:06 +0200

    Update Quote1Section.tsx

--- src/components/dashboard/Quote1Section.tsx
+++ src/components/dashboard/Quote1Section.tsx
@@ -27,7 +27,7 @@ const Quote1Section: React.FC = () => {
           <Quote className="w-6 h-6 text-yellow-400 flex-shrink-0 mt-1" />
           <div>
             <blockquote className="text-lg text-gray-700 font-medium italic leading-relaxed mb-3">
-              "While many writers worry about AI encroaching on authorship, a wave of startups sees opportunity — not to replace writers, they say, but to streamline the clutter around them."
+              "As generative AI takes on more of filmmaking’s creative process, a deeper question emerges: What does it mean when stories are shaped by a system that, for now at least, can’t feel -and whose users may not need it to?"
             </blockquote>
             <p className="font-semibold text-gray-900">- The Los Angeles Times 7/17 2025, interviewing many prominent industry people</p>
           </div>
