e33822de9b1ea51f560e17911f1fb6650a89fc27
QanticaTest <test@qantica.com>
2025-07-23 10:05:30 +0200

    Update Quote1Section.tsx

--- src/components/dashboard/Quote1Section.tsx
+++ src/components/dashboard/Quote1Section.tsx
@@ -24,7 +24,7 @@ const Quote1Section: React.FC = () => {
             <blockquote className="text-lg text-gray-700 font-medium italic leading-relaxed mb-3">
               "As generative AI takes on more of filmmaking’s creative process, a deeper question emerges: What does it mean when stories are shaped by a system that, for now at least, can’t feel -and whose users may not need it to?"
             </blockquote>
-            <p className="font-semibold text-gray-900">- The Los Angeles Times, Hollywood’s being reshaped by generative AI. What does that mean for screenwriters?/p>
+            <p className="font-semibold text-gray-900">- The Los Angeles Times, Hollywood’s being reshaped by generative AI. What does that mean for screenwriters?</p>
           </div>
         </div>
       </div>
