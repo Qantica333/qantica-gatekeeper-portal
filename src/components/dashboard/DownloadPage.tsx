cb6a10e90ef09db590e35d2679272afc514a7a3a
QanticaTest <test@qantica.com>
2025-07-23 09:47:48 +0200

    Update DownloadPage.tsx

--- src/components/dashboard/DownloadPage.tsx
+++ src/components/dashboard/DownloadPage.tsx
@@ -37,32 +37,6 @@ const DownloadPage: React.FC = () => {
         </Button>
 
       </div>
-
-           {/*   
-      <div className="bg-white rounded-lg p-6 shadow-sm border">
-        <div className="flex items-start gap-3">
-          <Quote className="w-6 h-6 text-yellow-400 flex-shrink-0 mt-1" />
-          <div>
-            <blockquote className="text-lg text-gray-700 font-medium italic leading-relaxed mb-3">
-              "While many writers worry about AI encroaching on authorship, a wave of startups sees opportunity — not to replace writers, they say, but to streamline the clutter around them."
-            </blockquote>
-            <p className="font-semibold text-gray-900">- The Los Angeles Times 7/17 2025, interviewing many prominent industry people</p>
-          </div>
-        </div>
-      </div>
-           
-      <div className="bg-white rounded-lg p-6 shadow-sm border">
-        <div className="flex items-start gap-3">
-          <Quote className="w-6 h-6 text-yellow-400 flex-shrink-0 mt-1" />
-          <div>
-            <blockquote className="text-lg text-gray-700 font-medium italic leading-relaxed mb-3">
-              "As generative AI takes on more of filmmaking’s creative process, a deeper question emerges: What does it mean when stories are shaped by a system that, for now at least, can’t feel -and whose users may not need it to?"
-            </blockquote>
-            <p className="font-semibold text-gray-900">- The Los Angeles Times 7/17 2025, interviewing many prominent industry people</p>
-          </div>
-        </div>
-      </div>
-          */}
     </div>
 
   );
