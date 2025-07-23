7780ebf01a1306ceaa457d68c68eaaec6ca69ca6
QanticaTest <test@qantica.com>
2025-07-23 10:04:49 +0200

    Update Quote2Section.tsx

--- src/components/dashboard/Quote2Section.tsx
+++ src/components/dashboard/Quote2Section.tsx
@@ -3,11 +3,6 @@ import React from 'react';
 import { Quote } from 'lucide-react';
 import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
 
-interface Quote2SectionProps {
-  titleOpacity: number;
-  titleTransform: string;
-}
-
 const Quote2Section: React.FC = () => {
   const { isVisible, elementRef } = useIntersectionObserver();
 
